699dbece2c4988765291f9580bd8407c1b9736d3
Dr. Saad Laouadi <data16scientist@gmail.com>
2023-05-13 20:58:00 +0300

    Add files via upload

---
+++ Practice-Files/05. Generating Random Vectors.r
@@ -0,0 +1,121 @@
+##*************************************************************************
+##*************************************************************************
+##                                                                       **
+##                       GENERATING RANDOM VECTORS                       **
+##                                                                       **
+##*************************************************************************
+##*************************************************************************
+
+
+# generate 1 random number between the default values of 0 and 1
+# Set the seed
+
+
+
+
+# generate 10 random numbers between 1 and 10
+# Set the seed 
+
+
+
+
+# generate 1000 random numbers between 1 and 10
+# Set the seed 
+
+
+
+
+# Check the summary statistics using the summary() function
+
+
+
+
+# generate 10 random number using the default of rnorm() function
+# Set the seed
+
+
+
+# Generate 25 random numbers from the normal distribution with mean=10 
+# and sd = 3.5
+# set the seed 
+
+
+# Check the summary statistics
+
+
+
+
+# Generate 500 random numbers from the normal distribution with mean=10 
+# and sd = 3.5
+# set the seed 
+
+
+
+# Check the summary statistics 
+
+
+
+
+# generate 1 random number from binomial distribution
+# set a seed for reproducibility (2)
+
+
+
+
+# Generate 10 random numbers from binomial distribution
+# Set the seed
+
+
+
+# Flip 10 coins 1 time (n=1, size = 10)
+# set the seed
+
+
+# Explain the result
+
+
+
+# Flip 10 coins with 10 times (n=10, size = 10)
+# Set the seed 
+
+
+
+
+# generate 10 random numbers from poisson distribution with lambda=2
+# Set the seed 
+
+
+
+
+# generate 500 random numbers from poisson distribution with lambda=7
+# Set the seed 
+
+
+
+# Get the summary statistics of the last vector
+
+
+
+# generate 10 random numbers from the exponential distribution with rate=2
+# Set the seed 
+
+
+
+# Generate 25 random numbers from the exponential distribution with rate=10
+# Set the seed 
+
+
+
+
+# Generate 500 random numbers from the exponential distribution with rate=3
+# Set the seed 
+
+
+
+
+# Get the summary statistics of the last vector
+
+
+
+
+
